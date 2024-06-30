532134099ca90bd7fdbf0abde1d0e6718e15d71b
appsecandrewlee <appsec.andrewlee@gmail.com>
2024-06-30 15:21:27 +1000

    trying again with the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
-import { HeaderNav } from "~/app/_components/header";
+import { HeaderNav } from "./_components/Header";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
